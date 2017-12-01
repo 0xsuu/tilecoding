2c1352d4a93298434c5bebc15f0cf7f016e04618
Hao <hs1145@york.ac.uk>
2017-12-01 20:40:00 +0000

    Change approch to make multiple outputs
    Now add 1 more input indicating which output this TC is predicting.
    
    Signed-off-by: Hao <hs1145@york.ac.uk>

--- tile_coder_tests.py
+++ tile_coder_tests.py
@@ -12,14 +12,12 @@ def test1():
 
     mtc_test1 = MultiTileCoder(dims, limits, 8, 2)
 
-    mtc_test1.tile_coders[0].tiles = np.array([1, 2, 3, 4])
-    mtc_test1.tile_coders[1].tiles = np.array([5, 6, 7, 8])
+    mtc_test1.tile_coder.tiles = np.array([1, 2, 3, 4])
 
     mtc_test2 = MultiTileCoder(dims, limits, 8, 2)
     mtc_test2.set_weights(mtc_test1.get_weights())
 
-    assert np.array_equal(mtc_test2.tile_coders[0].tiles, [1, 2, 3, 4])
-    assert np.array_equal(mtc_test2.tile_coders[1].tiles, [5, 6, 7, 8])
+    assert np.array_equal(mtc_test2.tile_coder.tiles, [1, 2, 3, 4])
 
     mtc_test1.save_weights("test.h5")
     mtc_test3 = MultiTileCoder(dims, limits, 8, 2)
@@ -27,8 +25,7 @@ def test1():
 
     os.remove("test.h5")
 
-    assert np.array_equal(mtc_test3.tile_coders[0].tiles, [1, 2, 3, 4])
-    assert np.array_equal(mtc_test3.tile_coders[1].tiles, [5, 6, 7, 8])
+    assert np.array_equal(mtc_test3.tile_coder.tiles, [1, 2, 3, 4])
 
 
 def test2():
@@ -49,11 +46,20 @@ def test3():
     limits = [(0, 1), (0, 1)]
 
     mtc_test1 = MultiTileCoder(dims, limits, 8, 2)
-    print(mtc_test1.train_on_batch(np.array([[0, 0]] * 1000), np.array([[1, 0]] * 1000)))
-    print(mtc_test1.train_on_batch(np.array([[1, 0]] * 1000), np.array([[0, 2]] * 1000)))
-    print(mtc_test1.train_on_batch(np.array([[0, 1]] * 1000), np.array([[0, 2]] * 1000)))
-    print(mtc_test1.train_on_batch(np.array([[1, 1]] * 1000), np.array([[0, 2]] * 1000)))
+    print(mtc_test1.train_on_batch(np.array([[0, 0]] * 500), np.array([[1, 0]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[1, 0]] * 500), np.array([[0, 2]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[0, 1]] * 500), np.array([[0, 2]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[1, 1]] * 500), np.array([[0, 2]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[0, 0]] * 500), np.array([[1, 0]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[1, 0]] * 500), np.array([[0, 2]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[0, 1]] * 500), np.array([[0, 2]] * 500)))
+    print(mtc_test1.train_on_batch(np.array([[1, 1]] * 500), np.array([[0, 2]] * 500)))
     prediction = np.round(mtc_test1.predict(np.array([[0, 0], [1, 0], [0, 1], [1, 1]]))).astype(int)
+    assert np.array_equal(prediction, [[1, 0], [0, 2], [0, 2], [0, 2]])
+
+    mtc_test2 = MultiTileCoder(dims, limits, 8, 2)
+    mtc_test2.set_weights(mtc_test1.get_weights())
+    prediction = np.round(mtc_test2.predict(np.array([[0, 0], [1, 0], [0, 1], [1, 1]]))).astype(int)
     assert np.array_equal(prediction, [[1, 0], [0, 2], [0, 2], [0, 2]])
 
 if __name__ == "__main__":
--- tilecoding.py
+++ tilecoding.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 import h5py
 from math import sqrt
@@ -10,55 +9,44 @@ SAVER_WEIGHTS_KEY = "tile_coding_tiles"
 class MultiTileCoder:
     def __init__(self, dims, limits, tilings, output_number, step_size=0.1, offset=lambda n: 2 * np.arange(n) + 1):
         self._output_numbers = output_number
-        self._tile_coders = []
-        for i in range(output_number):
-            self._tile_coders.append(TileCoder(dims, limits, tilings, step_size, offset))
+        new_dim = dims.copy()
+        new_dim.append(1)
+        new_limits = limits.copy()
+        new_limits.append((0, output_number))
+        self.tile_coder = TileCoder(new_dim, new_limits, tilings, step_size, offset)
 
     def predict(self, input_data):
         predictions = np.zeros((input_data.shape[0], self._output_numbers))
         for i in range(input_data.shape[0]):
             single_prediction = []
-            for t in self.tile_coders:
-                single_prediction.append(t[input_data[i]])
+            for action in range(self._output_numbers):
+                single_prediction.append(self.tile_coder[np.append(input_data[i], [action])])
             predictions[i] = single_prediction
         return predictions
 
     def train_on_batch(self, Xs, Ys):
         total_loss = 0
         for i in range(Xs.shape[0]):
-            for j in range(self._output_numbers):
-                total_loss += sqrt((Ys[i][j] - self._tile_coders[j][Xs[i]]) ** 2)
-                self._tile_coders[j][Xs[i]] = Ys[i][j]
+            for action in range(self._output_numbers):
+                total_loss += sqrt((Ys[i][action] - self.tile_coder[np.append(Xs[i], [action])]) ** 2)
+                self.tile_coder[np.append(Xs[i], [action])] = Ys[i][action]
         return total_loss
 
-    def get_weights(self) -> list:
-        weights_list = []
-        for t in self._tile_coders:
-            weights_list.append(t.tiles.copy())
-        return weights_list
+    def get_weights(self):
+        return self
 
-    def set_weights(self, weights: list):
-        # assert len(self._tile_coders) == len(weights)
-        for i in range(len(weights)):
-            self._tile_coders[i].tiles = weights[i]
+    def set_weights(self, weights):
+        self.tile_coder.tiles = weights.tile_coder.tiles.copy()
 
     def save_weights(self, file_path):
         h5_file = h5py.File(file_path, "w")
-        for i in range(len(self._tile_coders)):
-            h5_file.create_dataset(SAVER_WEIGHTS_KEY + str(i), data=self._tile_coders[i].tiles)
+        h5_file.create_dataset(SAVER_WEIGHTS_KEY, data=self.tile_coder.tiles)
         h5_file.close()
 
     def load_weights(self, file_path):
         h5_file = h5py.File(file_path, "r")
-        current_index = 0
-        while SAVER_WEIGHTS_KEY + str(current_index) in h5_file:
-            self._tile_coders[current_index].tiles = h5_file[SAVER_WEIGHTS_KEY + str(current_index)][:]
-            current_index += 1
+        self.tile_coder.tiles = h5_file[SAVER_WEIGHTS_KEY][:]
         h5_file.close()
-
-    @property
-    def tile_coders(self):
-        return self._tile_coders
 
 
 class TileCoder:
@@ -86,58 +74,3 @@ class TileCoder:
     def __setitem__(self, x, val):
         tile_ind = self._get_tiles(x)
         self.tiles[tile_ind] += self._alpha * (val - np.sum(self.tiles[tile_ind]))
-
-
-def example():
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    import time
-
-    # tile coder dimensions, limits, tilings, step size, and offset vector
-    dims = [8, 8]
-    lims = [(0, 2.0 * np.pi)] * 2
-    tilings = 8
-    alpha = 0.1
-
-    # create tile coder
-    T = tilecoder(dims, lims, tilings, alpha)
-
-    # target function with gaussian noise
-    def target_ftn(x, y, noise=True):
-        return np.sin(x) + np.cos(y) + noise * np.random.randn() * 0.1
-
-    # randomly sample target function until convergence
-    timer = time.time()
-    batch_size = 100
-    for iters in range(100):
-        mse = 0.0
-        for b in range(batch_size):
-            xi = lims[0][0] + np.random.random() * (lims[0][1] - lims[0][0])
-            yi = lims[1][0] + np.random.random() * (lims[1][1] - lims[1][0])
-            zi = target_ftn(xi, yi)
-            T[xi, yi] = zi
-            mse += (T[xi, yi] - zi) ** 2
-        mse /= batch_size
-        print('samples:', (iters + 1) * batch_size, 'batch_mse:', mse)
-    print('elapsed time:', time.time() - timer)
-
-    # get learned function
-    print('mapping function...')
-    res = 200
-    x = np.arange(lims[0][0], lims[0][1], (lims[0][1] - lims[0][0]) / res)
-    y = np.arange(lims[1][0], lims[1][1], (lims[1][1] - lims[1][0]) / res)
-    z = np.zeros([len(x), len(y)])
-    for i in range(len(x)):
-        for j in range(len(y)):
-            z[i, j] = T[x[i], y[j]]
-
-    # plot
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    X, Y = np.meshgrid(x, y)
-    surf = ax.plot_surface(X, Y, z, cmap=plt.get_cmap('hot'))
-    plt.show()
-
-
-if __name__ == '__main__':
-    example()
